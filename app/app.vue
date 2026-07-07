f29635d3551683e78c0caf9d33eb446e7f002a6a
Uzair-khane <uzairkhanar123@gmail.com>
2026-07-07 16:37:22 +0500

    update custom vision section with WhatsApp CTA

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,7 @@
 import  AppHeader  from "./components/common/AppHeader.vue";
 import  HeroSection  from "./components/home/HeroSection.vue";
 import ServicesSection from "./components/home/ServicesSection.vue";
+import SpecialVisionSection from "./components/home/SpecialVisionSection.vue";
 import WhyChooseSection from "./components/home/WhyChooseSection.vue";
 </script>
 <template>
@@ -13,6 +14,9 @@ import WhyChooseSection from "./components/home/WhyChooseSection.vue";
     </div>
     <div>
       <WhyChooseSection/>
+    </div>
+    <div>
+      <SpecialVisionSection/>
     </div>
   </div>
 </template>
